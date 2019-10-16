31b83bc688ad3c51a035d80105f13bf1747749ad
devrdias <devrdias@gmail.com>
2019-10-16 15:52:39 -0400

    list appointments

--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -3,6 +3,7 @@ import AppointmentController from '../app/controllers/AppointmentController';
 
 const router = new Router();
 
+router.get('/', AppointmentController.index);
 router.post('/', AppointmentController.store);
 
 export default router;
