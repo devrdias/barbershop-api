ccadca40e1156fb8ce1d6bb73b41320c7e61a197
devrdias <devrdias@gmail.com>
2019-10-16 23:19:29 -0400

    list available schedule for providers

--- src/routes/providers.js
+++ src/routes/providers.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import ProviderController from '../app/controllers/ProviderController';
+import AvailableController from '../app/controllers/AvailableController';
 
 const router = new Router();
 
 router.get('/', ProviderController.index);
+router.get('/:providerId/available', AvailableController.index);
 
 export default router;
